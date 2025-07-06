fb389680eaf6fa5f1ba42087b0ee769400d85023
Apolloxdevv <138712486+Apolloxdevv@users.noreply.github.com>
2025-07-06 15:17:09 +1000

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-// This script will redirect to the Roblox game page when the user clicks the button
-function playGame() {
-    window.location.href = "https://www.roblox.com/games/111111111/Baseplate-Game"; // Replace with your Roblox game URL
-}
