1b970a23aac0cb1fa445b96b4c9e1ebc20517b60
Apolloxdevv <138712486+Apolloxdevv@users.noreply.github.com>
2025-07-06 15:18:13 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// This script will redirect to the Roblox game page when the user clicks the button
+function playGame() {
+    window.location.href = "https://www.roblox.com/games/111111111/Baseplate-Game"; // Replace with your Roblox game URL
+}
